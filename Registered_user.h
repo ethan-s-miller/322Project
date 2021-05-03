57095dac15bee19a0e01372fecc7e6d2ee02e71d
cdellwo <connor.dellwo@wsu.edu>
2021-05-02 18:51:13 -0700

    added some functions to registered user class

--- Registered_user.h
+++ Registered_user.h
@@ -5,10 +5,36 @@ class Registered_user
 {
 private:
     std::string name;
+    std::string emailId;
+    std::string password;
+    std::vector<Books> booksIssued;
+    bool isLoggedIn = false;
+
+protected:
+
+    void setEmailId(std::string id) { this->emailId = id;}
+
+    void setPassword(std::string new_password) { this->password = new_password;}
+    
+    void setIsLoggedIn(bool status) { this->isLoggedIn = status;}
+    
+    
+
 
 public:
     Registered_user(std::string name);
     ~Registered_user();
+    
+    std::vector<Books> getBooksIssued() { return bookIssued;}
+    
+    std::string getName() {return name;}
+    
+    std::string getEmailId() {return emailId;}
+    
+    std::string getPassword() {return password;}
+    
+    
+    
 };
 
 Registered_user::Registered_user(std::string name)
